f43970eae13e9b15c04cec1765885f7f5fa75dbb
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-12-02 11:59:36 -0300

    added module webpack css config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import { getDataGitHub, Image } from './get-data-github'
 
-let cats;
-
-(async () => {
-  const data = await getDataGitHub()
-
-  console.log(data)
-
-  const image = new Image(data[0].url, data[0].id)
-
-  console.log(image.url)
-})()
-
+import './assets/global.css';
